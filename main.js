083aaa1d72a5e8970e3445f0a59addf7f84e48f5
Volodymyr Vodopshyn <84332962+VodopshinV@users.noreply.github.com>
2023-05-30 14:41:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -30,8 +30,8 @@ let alpha = 0,
     x,
     y,
     z;
-const EPSILON = 0.001;
-const MS2S = 1.0 / 1000.0;
+const EPSILON = 0.01;
+const MS2S = 1.0 / 10000.0;
 
 function Model(name) {
     this.name = name;
